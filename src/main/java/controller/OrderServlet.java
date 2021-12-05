fcee8fcddfefbb7defd51008436e931dc9695991
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-12-05 10:32:45 +0700

    update nhanh nqk

--- src/main/java/controller/OrderServlet.java
+++ src/main/java/controller/OrderServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "OrderServlet", value = "/order")
+@WebServlet(name = "OrderServlet", value = "/orders")
 public class OrderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
